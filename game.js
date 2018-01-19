fccf9e5ea13427b76168b7dcaed7e83df7fcb087
cory <939906498@qq.com>
2018-01-19 17:18:07 +0800

    start

--- game.js
+++ game.js
@@ -4,7 +4,6 @@ import './js/libs/symbol'
 //import Main from './js/main'
 //new Main()
 
-var context = canvas.getContext('2d');
-context.fillStyle = 'red';
-context.fillRect(0,0,100,100);
+import Start from './js/hexbomb/start'
 
+new Start();
