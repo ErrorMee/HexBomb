ad71dd8c3311e3545bcbdef38fd6952bb3116b78
cory <939906498@qq.com>
2018-01-19 15:28:53 +0800

    Initial commit

---
+++ game.js
@@ -0,0 +1,10 @@
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
+
+//import Main from './js/main'
+//new Main()
+
+var context = canvas.getContext('2d');
+context.fillStyle = 'red';
+context.fillRect(0,0,100,100);
+
